ff01648c8b4ac2caf1180287a646757c6a8e3d9e
Tias <tiasfenech@gmail.com>
2021-07-22 17:57:38 +0200

    Fix signalr to allow multiple games to be played simultaneously

--- src/App.Contracts/Interfaces/IGameHub.cs
+++ src/App.Contracts/Interfaces/IGameHub.cs
@@ -11,5 +11,6 @@ namespace App.Contracts.Interfaces
     {
         Task UpdateGame(Game game);
         Task SendEvent(IEvent evnt);
+        Task Connected(string connectionId);
     }
 }
--- src/App.Contracts/Interfaces/IGameService.cs
+++ src/App.Contracts/Interfaces/IGameService.cs
@@ -4,8 +4,8 @@ namespace App.Contracts.Interfaces
 {
     public interface IGameService
     {
-        Task CreateGame(int size, string gameName, string playerName);
-        Task ToggleLight(string gameName, int x, int y);
-        Task Replay(string gameName, int end);
+        Task CreateGame(string connectionId, int size, string gameName, string playerName);
+        Task ToggleLight(string connectionId, string gameName, int x, int y);
+        Task Replay(string connectionId, string gameName, int end);
     }
 }
